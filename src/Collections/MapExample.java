3ff3bd6a6ac034badbf1651481c6be867130438f
Maawiya06 <maawiyaameer001@gmail.com>
2025-06-14 15:33:11 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Collections/MapExample.java
+++ src/Collections/MapExample.java
@@ -11,5 +11,14 @@ public class MapExample {
         courses.put("Spring boot", 8000);
         courses.put("Androide", 4000);
         System.out.println(courses);
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++");
+        // by foreach
+        courses.forEach((e1, e2) ->{
+            System.out.println(e1+ "=>" +e2);
+        });
+
+        // for finding one element
+        System.out.println(courses.get("Core Java"));
     }
 }
