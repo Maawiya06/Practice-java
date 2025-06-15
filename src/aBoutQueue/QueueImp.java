fe0e3487d95982a68e98bc5eac14210208fdbc38
Maawiya06 <maawiyaameer001@gmail.com>
2025-06-15 18:37:14 +0530

    Merge remote-tracking branch 'origin/main'

--- src/aBoutQueue/QueueImp.java
+++ src/aBoutQueue/QueueImp.java
@@ -2,12 +2,22 @@ package aBoutQueue;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 public class QueueImp {
     public static void main(String args[]){
         Queue<Integer> q = new LinkedList<>();
         q.add(5);
         System.out.println(q);
+
+        System.out.println(q.size());
+
+        if(q.isEmpty() == true){
+            System.out.println("Queue is empty");
+        }
+        else{
+            System.out.println("Queue is not empty");
+        }
 
     }
 }
