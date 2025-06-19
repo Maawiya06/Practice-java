95a18784ae20551d1c62129d89a163eacbb80ef8
Maawiya06 <maawiyaameer001@gmail.com>
2025-06-19 11:27:50 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ src/aBoutQueue/FirstUniqueCharInString.java
@@ -0,0 +1,37 @@
+package aBoutQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FirstUniqueCharInString {
+    public static void main(String args[]) {
+        String str = "ababc";
+        Queue<Character> q = new LinkedList<>();
+        int[] freq = new int[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            freq[ch - 'a']++;
+            q.add(ch);
+
+            // answer find kro
+            while (!q.isEmpty()) {
+                char frontCharacter = q.peek();
+                if (freq[frontCharacter - 'a'] > 1) {
+                    // y answe nhi h
+                    q.poll();
+                } else {
+                    // ==1 wala case
+                    // yhi answer h
+                    System.out.println(frontCharacter);
+                    break;
+                }
+            }
+
+            if (q.isEmpty()) {
+                // puri Queue khali h
+                System.out.println("#");
+            }
+        }
+    }
+}
