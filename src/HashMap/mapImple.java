80aa0945d7b37efea9613410c414bcd5d8217529
Maawiya06 <maawiyaameer001@gmail.com>
2025-08-03 17:38:44 +0530

    Merge remote-tracking branch 'origin/main'

--- src/HashMap/mapImple.java
+++ src/HashMap/mapImple.java
@@ -1,11 +1,20 @@
 package HashMap;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class mapImple {
 
-    // map creation
-    Map<String, Integer> mapping = new HashMap<>();
+    public static void main(String[] args) {
+        // map creation
+        Map<String, Integer> mapping = new HashMap<>();
+
+        // insertion
+        mapping.put("Love", 25);
+        mapping.put("Ameer ", 10);
+
+        // print to verify
+        System.out.println(mapping);
+    }
 }
+
